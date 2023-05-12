3b64598cfda5e11ac1ead637f6e74b27cacdd2e3
Dawid Kolodziej <kolodziejdawid02@gmail.com>
2023-05-12 17:53:03 +0200

    Add exception handling to removing Google Calendar events

--- app/services/calendar_event_remover.rb
+++ app/services/calendar_event_remover.rb
@@ -11,9 +11,12 @@ class CalendarEventRemover
   def remove_event
     return unless user.token.present? && user.refresh_token.present?
 
-    google_calendar_client.delete_event(CALENDAR_ID, event_id)
+    begin
+      google_calendar_client.delete_event(CALENDAR_ID, event_id)
+    rescue StandardError => e
+      Rails.logger.debug e.message
+    end
   end
-
   private
 
   attr_reader :user, :event_id
